d38ffc008436f81b191bf4f7f841dc6d84181010
feycot <feycot@gmail.com>
2020-04-10 01:31:51 +0500

    fix seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
         $this->call(TaskStatusSeeder::class);
+        if (!env('production')) {
+            $this->call(UsersTableSeeder::class);
+        }
     }
 }
